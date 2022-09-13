41a230fac5ee4d4292c4f0e5f804135a0dcd55a8
Fabio <fabiodanille@hotmail.com>
2022-09-13 16:46:54 -0300

    Adicionando conteúdo

---
+++ Modulo-II-PythonParaCientistasDeDados/5-DominandoStrings-e-Fatiamento/4-StringMultiplasLinhas.py
@@ -0,0 +1,17 @@
+# String múltiplas linhas
+
+nome = "Guilherme"
+
+mensagem = f"""
+Olá meu nome é {nome},
+eu estou aprendendo Python.
+    Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+
+
+
+
+
