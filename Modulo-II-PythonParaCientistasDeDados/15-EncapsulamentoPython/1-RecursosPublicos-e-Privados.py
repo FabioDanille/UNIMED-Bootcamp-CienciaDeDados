1000bf96c941894499536c75955e40854977241d
Fabio <fabiodanille@hotmail.com>
2022-09-20 18:07:17 -0300

    Adicionando aula sobre Properties

---
+++ Modulo-II-PythonParaCientistasDeDados/15-EncapsulamentoPython/1-RecursosPublicos-e-Privados.py
@@ -0,0 +1,20 @@
+## Exemplo
+class Conta:
+    def __init__(self, nro_agencia, saldo=0):
+        self._saldo = saldo # PRIVADO, pois há um '_' antes
+        self.nro_agencia = nro_agencia
+    
+    def depositar(self, valor):
+        self._saldo += valor
+
+    def sacar(self, valor):
+        self._saldo += valor
+    
+    def mostrar_saldo(self):
+        return self._saldo
+
+
+conta = Conta("0001", 100)
+conta.depositar(100)
+print(conta.nro_agencia)
+print(conta.mostrar_saldo())
